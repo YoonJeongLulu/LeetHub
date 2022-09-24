4e25484912e4071009db78d8d2435def239bf3c9
Lulu <63350767+YoonJeongLulu@users.noreply.github.com>
2022-09-24 15:06:32 +0900

    Time: 0 ms (100.00%), Space: 42.1 MB (30.91%) - LeetHub

---
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.java
@@ -0,0 +1,26 @@
+class Solution {
+    int sum = 0;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        solve(root, 0);
+        return this.sum;
+    }
+
+    public void solve(TreeNode node, int dir){
+        
+        if(node == null) return;
+
+        if (node.left == null && node.right == null && dir==0) {
+            return;
+        }
+
+        if (node.left == null && node.right == null && dir==1) {
+            this.sum += node.val;
+            return;
+        }
+
+        solve(node.left, 1);
+        solve(node.right, 2);
+    }
+
+}
