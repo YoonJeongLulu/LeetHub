13f39c7eb604b1793278170404fd98ba0258ff8d
Lulu <63350767+YoonJeongLulu@users.noreply.github.com>
2022-09-24 15:57:26 +0900

    Time: 19 ms (44.47%), Space: 68.1 MB (78.03%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for (int num: nums){
+            if (map.containsKey(num)){
+                return true;
+            }
+            else {
+                map.put(num, 1);
+            }
+        }
+        return false;
+    }
+}
