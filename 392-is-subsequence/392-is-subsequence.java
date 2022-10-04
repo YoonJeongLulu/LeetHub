fb0dc43c88b05805a5be8e43f2998a47475b2cc7
Lulu <63350767+YoonJeongLulu@users.noreply.github.com>
2022-10-04 23:15:31 +0900

    Time: 15 ms (5.85%), Space: 44.1 MB (5.38%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -3,20 +3,17 @@ class Solution {
 
         String[] s_lt = s.split("");
         String[] t_lt = t.split("");
-        System.out.println(s.length());
-        
+
         if (s.length() == 0) return true;
 
-        int index = 0, cnt = 0;
+        int index = 0;
         for(String each_t: t_lt){
-           if (cnt == s_lt.length) return true;
+           if (index == s_lt.length) return true;
            if(s_lt[index].equals(each_t)){
-               System.out.println("index: " + index + " s: " + s_lt[index] + " t: " + each_t);
                index++;
-               cnt++;
            }
         }
-        if (cnt == s_lt.length) return true;
+        if (index == s_lt.length) return true;
         return false;
     }
 }
