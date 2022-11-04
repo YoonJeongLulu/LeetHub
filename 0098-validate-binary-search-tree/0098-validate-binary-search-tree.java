eb098bd0852c0fa25ff17d9d0cb5ec740721b483
Lulu <63350767+YoonJeongLulu@users.noreply.github.com>
2022-11-04 11:33:20 +0900

    Time: 1 ms (64.69%), Space: 44.1 MB (36.49%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -4,16 +4,14 @@ class Solution {
   }
 
   public boolean isValidBST(TreeNode node, Integer minLimit, Integer maxLimit) {
-     
-    if (node == null) return true;
+      
+      if (node == null) return true;
+    
+      if (minLimit != null && node.val <= minLimit) return false;
+      if (maxLimit != null && node.val >= maxLimit) return false;
+      
+      return isValidBST(node.left, minLimit, node.val) && isValidBST(node.right, node.val, maxLimit);
 
-    if (minLimit != null && node.val <= minLimit) return false;
-    if (maxLimit != null && node.val >= maxLimit) return false;
-
-    if (!isValidBST(node.left, minLimit, node.val)) return false;
-    if (!isValidBST(node.right, node.val, maxLimit)) return false;
-
-    return true;
   }
 
 }
