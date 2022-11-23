e22584a7fb6b7002a882a31d1f95cfc04d03283b
Lulu <63350767+YoonJeongLulu@users.noreply.github.com>
2022-11-23 13:44:34 +0900

    Time: 15 ms (95.41%), Space: 13.4 MB (69.17%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,48 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        
+        n = len(matrix)
+        m = len(matrix[0])
+        
+        def go_outside(x, y):
+            if x < 0 or x >= n or y < 0 or y >= m:
+                return True
+            return False
+        
+        visited = [[False] * m for _ in range(n)]
+        
+        end = 0
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        
+        x = 0
+        y = 0
+        
+        ans = [matrix[0][0]]
+        visited[0][0] = True
+        
+        idx = 0
+        while (True):
+            if (end == n*m-1):
+                break
+            
+            dx = directions[idx%4][0]
+            dy = directions[idx%4][1]
+            print(idx)
+            
+            if (go_outside(x+dx, y+dy)):
+                idx += 1
+                continue
+            if (visited[x+dx][y+dy]):
+                idx += 1
+                continue
+            x = x + dx
+            y = y + dy
+            ans.append(matrix[x][y])
+            print(ans)
+            visited[x][y] = True
+            end += 1
+            
+        return ans
+            
+
+        
